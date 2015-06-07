748fe4c2b7b24ac350e47fb323ed58957e171a1f
Matthew Wells <matthewpwells@gmail.com>
2015-06-06 17:42:00 -0700

    Tore it down and started again

--- js/app.js
+++ js/app.js
@@ -25,69 +25,22 @@ var NYMap = {
 
 var model = function(data) {
 	this.lat = ko.observable(data.lat);
-	this.lng = ko.observable(data.lng);
-	this.zoom = ko.observable(data.zoom);
-	this.vis = ko.observable(data.vis);
-	this.markers = ko.computed(function() { 
-	 for (marker in data.NYMarkers) {	
-	 	if (searchBox() === "d" || searchBox() === data.NYMarkers[marker].name) {
-	 		data.NYMarkers[marker].vis = false;
-	 	}}
-	 	return data.NYMarkers;
-	});
-	
 }
 
-var viewModel = function() {
-	self = this;
-	this.counter = function() {
-		console.log(3);
-	};
+var viewModel = function() { 
 
-	searchBox = ko.observable("");
-	mapData = new model(NYMap);
-
-	self.myMap = ko.observable({
-		lat: mapData.lat(),
-		lng: mapData.lng(),
-		zoom: mapData.zoom(),
-		markers: mapData.markers(),
-		vis: mapData.vis()
-	});
+	myMap = new model(NYMap);
 }
 
-
-ko.bindingHandlers.map = {
-	//create mapview centered over specified area
-	update: function(element, valueAccesor) {
-		var lat = ko.utils.unwrapObservable(valueAccesor()).lat;
-		var lng = ko.utils.unwrapObservable(valueAccesor()).lng;
-		var zoom = ko.utils.unwrapObservable(valueAccesor()).zoom;
-		var latLng = new google.maps.LatLng(parseFloat(lat),parseFloat(lng));
-		var markers = ko.utils.unwrapObservable(valueAccesor()).markers;
-		var mapOptions = {
-			center: latLng,
-			zoom: zoom
-		};
-		var map = new google.maps.Map(element,mapOptions);
-
-		//function to place marker
-		var makeMarker = function(location) {
-			var markerLatLng = new google.maps.LatLng(parseFloat(location.lat), 
-				parseFloat(location.lng));
-			var marker = new google.maps.Marker({
-				position: markerLatLng,
-				map: map,
-			})
-			marker.setVisible(location.vis);
-		};
-
-		//itterates over all markers in array
-		markers.forEach(function(e) {
-			makeMarker(e)
-		});
-
+ko.bindingHandlers.mapper = {
+	update: function(element, valueAccessor) {
+        var mapOptions = {
+          center: { lat: -34.397, lng: 150.644},
+          zoom: 8
+        };
+        var map = new google.maps.Map(document.getElementById('map-canvas'),
+            mapOptions);
 	}
-}
+};
 
 ko.applyBindings(new viewModel());
